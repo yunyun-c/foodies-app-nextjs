a7aff5c0300016873c9c70cbe2fb965285522fc6
cassie_c <cassie.nightingale.13.13@gmail.com>
2024-01-24 13:31:04 -0600

    course 106

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,26 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
 function Meals() {
   return (
-    <div>
-      <h1>The Meals</h1>
-    </div>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p className={classes.cta}>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
 
