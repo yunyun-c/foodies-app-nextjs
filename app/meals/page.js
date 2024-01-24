679a74123bed300f9df9a17d2679780daf190e44
cassie_c <cassie.nightingale.13.13@gmail.com>
2024-01-24 14:06:39 -0600

    course 107-110

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,10 @@
 import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
 
-function Meals() {
+async function Meals() {
+  const meals = await getMeals();
   return (
     <>
       <header className={classes.header}>
@@ -18,7 +20,7 @@ function Meals() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
