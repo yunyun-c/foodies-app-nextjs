db139a7bc53eac7c10646ae37269abfdb7612c5d
cassie_c <cassie.nightingale.13.13@gmail.com>
2024-01-24 21:33:29 -0600

    course 111

---
+++ app/meals/error.js
@@ -0,0 +1,11 @@
+"use client";
+function Error() {
+  return (
+    <main className="error">
+      <h1>An error occurred!</h1>
+      <p>Failed to fetch meal data. Please try again later.</p>
+    </main>
+  );
+}
+
+export default Error;
